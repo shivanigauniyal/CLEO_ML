23bfc7be7d51ae751721fa8110ca553676bf283e
Clara Levante <yoctoyotta1024@yahoo.com>
2023-08-21 17:37:52 +0200

    reverted binary creation scripts to previous condition

--- create_gbxboundariesbinary_script.py
+++ create_gbxboundariesbinary_script.py
@@ -6,30 +6,38 @@ from pySD.gbxboundariesbinary_src.create_gbxboundaries import *
 from pySD.gbxboundariesbinary_src.read_gbxboundaries import *
 
 ### ----------------------- INPUT PARAMETERS ----------------------- ###
-### Essential Absolute or Relative Paths and Filenames ###
-path2CLEO     = sys.argv[1]
-path2build    = sys.argv[2]
-configfile    = sys.argv[3]
-constsfile    = path2CLEO+"libs/claras_SDconstants.hpp"
-binariespath  = path2build+"/share/"
-savefigpath   = path2build+"/bin/"
-gridfile      = binariespath+"/dimlessGBxboundaries.dat" # Note: this should match config_oneD.txt
+### absolute or relative paths for build and CLEO directories
+path2CLEO = sys.argv[1]
+path2build = sys.argv[2]
+configfile = sys.argv[3]
 
-### Booleans for Plotting Figures of Initial Conditions ###
-isfigures      = [True, True]                               # [making+showing, saving]
+### booleans for [making+showing, saving] figures
+isfigures = [True, True]
 
-### input parameters for coordinates of gridbox boundaries
-zmax          = 5000     # maximum z coord [m]
-zmin          = 0        # minimum z coord [m]
-zdelta        = 20     # uniform gridbox spacing [m]
-zgrid         = [zmin, zmax, zdelta] 
+### essential paths and filenames
+constsfile = path2CLEO+"libs/claras_SDconstants.hpp"
+binariespath = path2build+"/share/"
+savefigpath = path2build+"/bin/"
 
-xgrid         = np.asarray([0, 20]) # [m]
-ygrid         = np.asarray([0, 20]) # [m]
+gridfile =  binariespath+"/dimlessGBxboundaries.dat" # note this should match config.txt
+
+### input parameters for zcoords of gridbox boundaries
+zmax = 100 # maximum z coord [m]
+zmin = 0 # minimum z coord [m]
+zdelta = 100 # even spacing
+zgrid = [zmin, zmax, zdelta] 
+
+### input parameters for x coords of gridbox boundaries
+xgrid = [0, 1, 100]
+
+### input parameters for y coords of gridbox boundaries
+ygrid = np.asarray([0, 100])
+# ygrid = [0, 1500, 200]
 ### ---------------------------------------------------------------- ###
 
+
 ### -------------------- BINARY FILE GENERATION--------------------- ###
-### ensure build, share and bin directories exist ###
+### ensure build, share and bin directories exist
 if path2CLEO == path2build:
   raise ValueError("build directory cannot be CLEO")
 else:
@@ -37,14 +45,12 @@ else:
   Path(binariespath).mkdir(exist_ok=True) 
 
 ### write gridbox boundaries binary
-write_gridboxboundaries_binary(gridfile, zgrid, 
-                               xgrid, ygrid, constsfile)
+write_gridboxboundaries_binary(gridfile, zgrid, xgrid, ygrid, constsfile)
 print_domain_info(constsfile, gridfile)
 
 ### plot gridbox boundaries binary
 if isfigures[0]:
   if isfigures[1]:
     Path(savefigpath).mkdir(exist_ok=True) 
-  plot_gridboxboundaries(constsfile, gridfile,
-                         savefigpath, isfigures[1])
+  plot_gridboxboundaries(constsfile, gridfile, savefigpath, isfigures[1])
 ### ---------------------------------------------------------------- ###
